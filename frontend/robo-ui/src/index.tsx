0b1a885d953c18336092a58bc4ec6d00244e0189
Marcos Xavier <166416814+marcosxf-ciandt@users.noreply.github.com>
2024-07-31 22:51:59 -0300

    corrigir projeto frontend
    recriado projeto frontend para envia-lo para o github

--- frontend/robo-ui/src/index.tsx
+++ frontend/robo-ui/src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Robo from './components/Robo';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Robo from "./components/Robo";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
